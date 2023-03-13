23fee861e8c5b28ff168e2c9a66485b6be8c6b39
Tagain <76603136+Tagain@users.noreply.github.com>
2023-03-13 19:45:58 +0100

    Add files via upload

---
+++ abstract/abstract.js
@@ -0,0 +1,15 @@
+// window.onload = function () {
+//     window.addEventListener('scroll', function (e) {
+//         if (window.pageyOffset > 100) {
+//             document.getElementById('header').classList.add('is-scrolling');
+//         } else {
+//             document.getElementById('header').classList.remove('is-scrolling');
+//         }
+//     });
+
+//     const menu_btn = document.querySelector('.hamburger');
+
+//     menu_btn.addEventListener('click', function () {
+//         menu_btn.classList.toggle('is-active');
+//     });
+// }
